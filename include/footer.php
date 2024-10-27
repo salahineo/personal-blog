cd5a9fd6db144e73e5c07ca5fa96da9cedb2fd35
Mohamed Salah <salahineo.personal@gmail.com>
2024-10-27 22:16:11 +0300

    fix: refactor project

--- include/footer.php
+++ include/footer.php
@@ -1,29 +1,9 @@
   <!-- Start Footer -->
   <footer class="bg-dark text-light">
     <div class="container">
-      <div class="row d-flex flex-column flex-md-row align-items-center">
-        <div class="copyright col-md-6 mb-2 mb-md-0 text-center text-md-right">
-          &copy; <span id="footer-copyright-year"></span> | <span class="text-primary font-weight-bold">Mohamed Salah</span>
-        </div>
-        <div class="links col-md-6 text-center text-md-left">
-          <a href="https://github.com/salahineo" target="_blank">
-            <i class="fab fa-github"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/salahineo/" target="_blank">
-            <i class="fab fa-linkedin-in"></i>
-          </a>
-          <a href="https://salahineo.github.io/salahineo/" target="_blank">
-            <i class="fas fa-globe-africa"></i>
-          </a>
-          <a href="https://twitter.com/salahineo" target="_blank">
-            <i class="fab fa-twitter"> </i>
-          </a>
-          <a href="https://www.facebook.com/salahineo/" target="_blank">
-            <i class="fab fa-facebook-f"></i>
-          </a>
-          <a href="mailto:salahineo.work@gmail.com" target="_blank">
-            <i class="fas fa-envelope"></i>
-          </a>
+      <div class="row d-flex flex-column flex-md-row align-items-center justify-content-center">
+        <div class="copyright col-md-6 mb-2 mb-md-0 text-center">
+          &copy; <span id="footer-copyright-year"></span> | <a href="https://salahineo.com" target="_blank" rel="noopener" class="text-primary font-weight-bold">Mohamed Salah</a>
         </div>
       </div>
     </div>
